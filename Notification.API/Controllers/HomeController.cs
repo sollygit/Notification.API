99d8157fb936555a3f7d25702747b71f157bfe2c
Solly <mail4solly@gmail.com>
2020-06-06 00:14:48 +1000

    add default route

---
+++ Notification.API/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Notification.API.Controllers
+{
+    [Route("")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectPermanent("/swagger/index.html");
+        }
+    }
+}
